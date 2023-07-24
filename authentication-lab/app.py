b929a77ab3837c8eb86a94b5302c70acf19ea9e7
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2023-07-24 12:15:06 +0300

    Done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -11,12 +11,14 @@ Config = {
   "messagingSenderId": "276151677934",
   "appId": "1:276151677934:web:7499ec07edc5b49f7514ff",
   "measurementId": "G-RE91HDNEVT",
-  "databaseURL": ""
+  "databaseURL": "https://originalname-4859a-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 
 firebase = pyrebase.initialize_app(Config)
 auth = firebase.auth()
+db = firebase.database()
+
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -43,9 +45,15 @@ def signup():
     if(request.method == "POST"):
        email =  request.form["email"]
        password = request.form["password"]
+       name = request.form["name"]
+       username = request.form["username"]
+       bio = request.form["bio"]
 
        try:
            login_session["user"] = auth.create_user_with_email_and_password(email,password)
+           UID = login_session["user"]["localId"]
+           user = {"email" : email, "password": password, "name": name, "username":username,"bio":bio}
+           db.child("Users").child(UID).set(user)
            return redirect(url_for('add_tweet'))
        except:
            error = 'auth error'
@@ -58,9 +66,22 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+    if(request.method == "POST"):
+        title = request.form['title']
+        text = request.form['text']
+        tweet = {"title": title, "text": text, "uid":login_session["user"]["localId"] }
+        
+        db.child("Tweets").push(tweet)
 
+        return redirect(url_for('all_tweets'))
+    else:
+        return render_template("add_tweet.html")
 
+@app.route('/all_tweets')
+def all_tweets():
+    
+    all_tweets = db.child("Tweets").get().val()
+    return render_template("tweets.html", tweets=all_tweets)
 
 
 if __name__ == '__main__':
